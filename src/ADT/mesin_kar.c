6f8e66c6d5c74eb4562482e42f7c3a964527ab1f
zhillan Attarizal-R <70697271+zhillan-arf@users.noreply.github.com>
2021-11-22 20:30:08 +0700

    Edit2 mesinkar mesinkata biar bisa nerima filename, tapi cek lagi coba

--- src/ADT/mesin_kar.c
+++ src/ADT/mesin_kar.c
@@ -10,7 +10,7 @@ boolean EOP;
 static FILE * pita;
 static int retval;
 
-void START(char filename) {
+void START(char *filename) {
 /* Mesin siap dioperasikan. Pita disiapkan untuk dibaca.
    Karakter pertama yang ada pada pita posisinya adalah pada jendela.
    I.S. : sembarang
